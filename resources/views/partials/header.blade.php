128a7affac90c808a33a71e2dc81d466f0f67cd0
Dylan Altamirano <doaltamiranoce@est.utn.ac.cr>
2018-07-18 21:32:29 -0600

    cambios en el encabezado de la página principal

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,38 @@
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Ecolones</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
+            aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#about">Acerca</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#services">Centros de Acopio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#contact">Contactenos</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+{{--END of Navigation--}}
+
+{{--HEADER--}}
+
+<header class="bg-primary text-white">
+    <div class="container text-center">
+        <h1>Bienvenido al sitio principal de la moneda ecológica de Costa Rica</h1>
+        <p class="lead">En esta página encontraras todo lo que necesitas saber para comenzar a obtener ecomonedas. Es
+            muy simple y, práctico. Siéntase en libertad de explorar nuestras opciones antes de tomar una decisión.
+        </p>
+    </div>
+</header>
+
+{{--END of HEADER--}}

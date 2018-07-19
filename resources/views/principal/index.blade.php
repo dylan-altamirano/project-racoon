0087cbc1c38baec6c4b65cc77d6c6e6116c3ebb6
Dylan Altamirano <doaltamiranoce@est.utn.ac.cr>
2018-07-18 22:10:51 -0600

    cambios en la ruta del proyecto

---
+++ resources/views/principal/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('titulo', 'La modena ecológica de Costa Rica')
+
+@section('contenido')
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <h2>Ecolones</h2>
+                    <p class="lead">Ya llegó a nuestro país, una nueva forma de hacer dinero. Estos son los beneficios con los 
+                        que contamos:
+                    </p>
+                    <ul>
+                        <li>Ayudas con el ambiente</li>
+                        <li>Tenés dinero disponible con poca inversión</li>
+                        <li>Inpiras a otras personas a ayudar con la causa también</li>
+                        <li>Sos un importante factor del cambio para nuestro planeta</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection

488157729526114b4002f71c0763b23d740fcfd6
Rodriguez Barquero <cielo.rodriguez.barquero@intel.com>
2018-08-19 12:45:31 -0600

    Actualizar usuarios listo

--- public/js/custom-script.js
+++ public/js/custom-script.js
@@ -34,3 +34,44 @@ $(".show").on('click','.buscarCliente',function(e){
         }
     });
 });
+
+/*
+*  Función Ajax para traer un admin centro acopio de la base de datos 
+*/
+$(".show").on('click','.buscarAdmin',function(e){
+
+    e.preventDefault();
+
+    $.ajax({
+        type:'POST',
+        url:'/auth/show',
+        datatype:'json',
+        data:{
+            '_token':$('input[name=_token]').val(),
+            'emailCliente':$('#emailCliente').val()
+        },
+        success:function(data){
+            if ((data.errors)) {
+                
+                $.each(data.errors, function(key, value){
+                    $('.alert-danger').show();
+                    $('.alert-danger').append('<p>'+value+'</p>');
+                });
+            }else{
+
+                if (data.name != null) {
+                    $('#lblNombreCliente').text(data.name);
+                    $('#cliente_id').val(data.id);
+                    $('#name').val(data.name);
+                    $('#direccion').val(data.direccion);
+                    $('#telefono').val(data.telefono);
+                    $('#email').val(data.email);
+                    
+                }else{
+                    $('#lblNombreCliente').text('No existe el usuario');
+                }
+               
+            }
+        }
+    });
+});
